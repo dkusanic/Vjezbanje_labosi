ceb2156f2a19d00f2a9b233a6980fcbebd979f5c
Domagoj Kusanic <dkusanic@foi.hr>
2018-12-06 17:42:24 +0100

    forma dizajnirana
    dodane kontrole na formu, poceo sa izradom logike

--- semafor/semafor/semafor/Form1.cs
+++ semafor/semafor/semafor/Form1.cs
@@ -16,5 +16,26 @@ namespace semafor
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.Red;
+        }
+
+        private void OsvjeziSemafor()
+        {
+            timer1.Start();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OsvjeziSemafor();
+        }
     }
 }
---
+++ semafor/semafor/semafor/Semafor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semafor
+{
+    class Semafor
+    {
+        public Semafor()
+        {
+
+        }
+
+        public int trenutnoStanje { get; set; }
+        public string[] stanja { get; set; }
+        public bool silazno { get; set; }
+        public void PromijeniStanje()
+        {
+
+        }
+        public string DohvatiTrenutnoStanje()
+        {
+            string s = null;
+            return s;
+        }
+    }
+}
