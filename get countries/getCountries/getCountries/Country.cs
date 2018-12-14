1e3e96900694532630504181a20bab922cee6e94
Domagoj Kusanic <dkusanic@foi.hr>
2018-12-14 14:52:39 +0100

    kreirana klasa Country
    potrebno jos metode napraviti

--- get countries/getCountries/getCountries/Country.cs
+++ get countries/getCountries/getCountries/Country.cs
@@ -9,5 +9,24 @@ namespace getCountries
 {
     class Country
     {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private int Population { get; set; }
+        private int Area { get; set; }
+        private float PopulationDensity { get; set; }
+        public Country(int id, string name, int population, int area)
+        {
+
+        }
+
+        public float CalculatePopulationDensity()
+        {
+            return ;
+        }
+
+        public static List<Country> GetCountries(List<string> countries)
+        {
+
+        }
     }
 }
