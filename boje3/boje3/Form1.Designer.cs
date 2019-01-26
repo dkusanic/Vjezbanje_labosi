e093e3f1cfa32fab554833dfe86edc596c984a96
Domagoj Kusanic <dkusanic@foi.hr>
2019-01-26 11:36:31 +0100

    kreiran projekt boje3 i napravljen dizajn forme

---
+++ boje3/boje3/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace boje3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelRed = new System.Windows.Forms.Label();
+            this.labelGreen = new System.Windows.Forms.Label();
+            this.labelBlue = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBoxRed = new System.Windows.Forms.TextBox();
+            this.textBoxGreen = new System.Windows.Forms.TextBox();
+            this.textBoxBlue = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBoxBoje = new System.Windows.Forms.ComboBox();
+            this.panelOdabranaBoja = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonAdd);
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Controls.Add(this.textBoxBlue);
+            this.groupBox1.Controls.Add(this.textBoxGreen);
+            this.groupBox1.Controls.Add(this.textBoxRed);
+            this.groupBox1.Controls.Add(this.labelName);
+            this.groupBox1.Controls.Add(this.labelBlue);
+            this.groupBox1.Controls.Add(this.labelGreen);
+            this.groupBox1.Controls.Add(this.labelRed);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(176, 182);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add new color";
+            // 
+            // labelRed
+            // 
+            this.labelRed.AutoSize = true;
+            this.labelRed.Location = new System.Drawing.Point(11, 31);
+            this.labelRed.Name = "labelRed";
+            this.labelRed.Size = new System.Drawing.Size(51, 13);
+            this.labelRed.TabIndex = 0;
+            this.labelRed.Text = "Red . . . .";
+            // 
+            // labelGreen
+            // 
+            this.labelGreen.AutoSize = true;
+            this.labelGreen.Location = new System.Drawing.Point(11, 60);
+            this.labelGreen.Name = "labelGreen";
+            this.labelGreen.Size = new System.Drawing.Size(51, 13);
+            this.labelGreen.TabIndex = 1;
+            this.labelGreen.Text = "Green  . .";
+            // 
+            // labelBlue
+            // 
+            this.labelBlue.AutoSize = true;
+            this.labelBlue.Location = new System.Drawing.Point(11, 90);
+            this.labelBlue.Name = "labelBlue";
+            this.labelBlue.Size = new System.Drawing.Size(52, 13);
+            this.labelBlue.TabIndex = 2;
+            this.labelBlue.Text = "Blue . . . .";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(9, 120);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(53, 13);
+            this.labelName.TabIndex = 3;
+            this.labelName.Text = "Name . . .";
+            // 
+            // textBoxRed
+            // 
+            this.textBoxRed.Location = new System.Drawing.Point(63, 28);
+            this.textBoxRed.Name = "textBoxRed";
+            this.textBoxRed.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRed.TabIndex = 4;
+            // 
+            // textBoxGreen
+            // 
+            this.textBoxGreen.Location = new System.Drawing.Point(63, 57);
+            this.textBoxGreen.Name = "textBoxGreen";
+            this.textBoxGreen.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGreen.TabIndex = 5;
+            // 
+            // textBoxBlue
+            // 
+            this.textBoxBlue.Location = new System.Drawing.Point(63, 87);
+            this.textBoxBlue.Name = "textBoxBlue";
+            this.textBoxBlue.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBlue.TabIndex = 6;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(63, 117);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 7;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(63, 143);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(100, 23);
+            this.buttonAdd.TabIndex = 8;
+            this.buttonAdd.Text = "Add color";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.panelOdabranaBoja);
+            this.groupBox2.Controls.Add(this.comboBoxBoje);
+            this.groupBox2.Location = new System.Drawing.Point(194, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(176, 182);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Preview";
+            // 
+            // comboBoxBoje
+            // 
+            this.comboBoxBoje.FormattingEnabled = true;
+            this.comboBoxBoje.Location = new System.Drawing.Point(6, 23);
+            this.comboBoxBoje.Name = "comboBoxBoje";
+            this.comboBoxBoje.Size = new System.Drawing.Size(164, 21);
+            this.comboBoxBoje.TabIndex = 0;
+            // 
+            // panelOdabranaBoja
+            // 
+            this.panelOdabranaBoja.Location = new System.Drawing.Point(6, 87);
+            this.panelOdabranaBoja.Name = "panelOdabranaBoja";
+            this.panelOdabranaBoja.Size = new System.Drawing.Size(164, 89);
+            this.panelOdabranaBoja.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 208);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PI Color Adding";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxBlue;
+        private System.Windows.Forms.TextBox textBoxGreen;
+        private System.Windows.Forms.TextBox textBoxRed;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelBlue;
+        private System.Windows.Forms.Label labelGreen;
+        private System.Windows.Forms.Label labelRed;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel panelOdabranaBoja;
+        private System.Windows.Forms.ComboBox comboBoxBoje;
+    }
+}
+
