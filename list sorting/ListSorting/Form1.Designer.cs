ace9f8b62d284f6e74006608d6d8533cad5bbc2a
Domagoj Kusanic <dkusanic@foi.hr>
2019-02-08 09:54:54 +0100

    dizajnirana forma aplikacije

--- list sorting/ListSorting/Form1.Designer.cs
+++ list sorting/ListSorting/Form1.Designer.cs
@@ -28,13 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBoxData = new System.Windows.Forms.GroupBox();
+            this.labelNameLastName = new System.Windows.Forms.Label();
+            this.textBoxNameLastName = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.groupBoxSortMethod = new System.Windows.Forms.GroupBox();
+            this.buttonSortName = new System.Windows.Forms.Button();
+            this.buttonSortLastname = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.listBoxPopis = new System.Windows.Forms.ListBox();
+            this.groupBoxData.SuspendLayout();
+            this.groupBoxSortMethod.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxData
+            // 
+            this.groupBoxData.Controls.Add(this.buttonAdd);
+            this.groupBoxData.Controls.Add(this.textBoxNameLastName);
+            this.groupBoxData.Controls.Add(this.labelNameLastName);
+            this.groupBoxData.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxData.Name = "groupBoxData";
+            this.groupBoxData.Size = new System.Drawing.Size(330, 57);
+            this.groupBoxData.TabIndex = 0;
+            this.groupBoxData.TabStop = false;
+            this.groupBoxData.Text = "Data";
+            // 
+            // labelNameLastName
+            // 
+            this.labelNameLastName.AutoSize = true;
+            this.labelNameLastName.Location = new System.Drawing.Point(19, 25);
+            this.labelNameLastName.Name = "labelNameLastName";
+            this.labelNameLastName.Size = new System.Drawing.Size(113, 13);
+            this.labelNameLastName.TabIndex = 0;
+            this.labelNameLastName.Text = "Name and Last Name:";
+            // 
+            // textBoxNameLastName
+            // 
+            this.textBoxNameLastName.Location = new System.Drawing.Point(138, 22);
+            this.textBoxNameLastName.Name = "textBoxNameLastName";
+            this.textBoxNameLastName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNameLastName.TabIndex = 1;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(244, 20);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxSortMethod
+            // 
+            this.groupBoxSortMethod.Controls.Add(this.buttonClear);
+            this.groupBoxSortMethod.Controls.Add(this.buttonSortLastname);
+            this.groupBoxSortMethod.Controls.Add(this.buttonSortName);
+            this.groupBoxSortMethod.Location = new System.Drawing.Point(12, 75);
+            this.groupBoxSortMethod.Name = "groupBoxSortMethod";
+            this.groupBoxSortMethod.Size = new System.Drawing.Size(330, 57);
+            this.groupBoxSortMethod.TabIndex = 1;
+            this.groupBoxSortMethod.TabStop = false;
+            this.groupBoxSortMethod.Text = "Sort Method";
+            // 
+            // buttonSortName
+            // 
+            this.buttonSortName.Location = new System.Drawing.Point(6, 19);
+            this.buttonSortName.Name = "buttonSortName";
+            this.buttonSortName.Size = new System.Drawing.Size(75, 23);
+            this.buttonSortName.TabIndex = 0;
+            this.buttonSortName.Text = "By Name";
+            this.buttonSortName.UseVisualStyleBackColor = true;
+            // 
+            // buttonSortLastname
+            // 
+            this.buttonSortLastname.Location = new System.Drawing.Point(119, 19);
+            this.buttonSortLastname.Name = "buttonSortLastname";
+            this.buttonSortLastname.Size = new System.Drawing.Size(89, 23);
+            this.buttonSortLastname.TabIndex = 1;
+            this.buttonSortLastname.Text = "By Lastname";
+            this.buttonSortLastname.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(244, 19);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "Clear List";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // listBoxPopis
+            // 
+            this.listBoxPopis.FormattingEnabled = true;
+            this.listBoxPopis.Location = new System.Drawing.Point(12, 138);
+            this.listBoxPopis.Name = "listBoxPopis";
+            this.listBoxPopis.Size = new System.Drawing.Size(330, 160);
+            this.listBoxPopis.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(355, 313);
+            this.Controls.Add(this.listBoxPopis);
+            this.Controls.Add(this.groupBoxSortMethod);
+            this.Controls.Add(this.groupBoxData);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "List Sorting";
+            this.groupBoxData.ResumeLayout(false);
+            this.groupBoxData.PerformLayout();
+            this.groupBoxSortMethod.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxData;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxNameLastName;
+        private System.Windows.Forms.Label labelNameLastName;
+        private System.Windows.Forms.GroupBox groupBoxSortMethod;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonSortLastname;
+        private System.Windows.Forms.Button buttonSortName;
+        private System.Windows.Forms.ListBox listBoxPopis;
     }
 }
 
