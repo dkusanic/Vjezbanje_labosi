dbc7c71a5594682a0036fd4c4af5c7c48113871f
Domagoj Kusanic <dkusanic@foi.hr>
2019-02-09 12:12:02 +0100

    kreiran projekt zaposlenici
    i napravljena klasa Zaposlenik i slozen unos novog zaposlenika

---
+++ zaposlenici/zaposlenici/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zaposlenici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Zaposlenik> listaZaposlenika = new List<Zaposlenik>();
+            int izbor = 0;
+            int id = 1;
+            do
+            {
+                Console.Clear();
+                Console.Write("- - - I Z B O R N I K - - -\n" +
+                    "1. Dodavanje zaposlenika\n" +
+                    "2. Dodavanje plaće zaposleniku (već unesenom)\n" +
+                    "3. Pretraživanje i ispis podataka zaposlenika po imenu i/ili ID\n" +
+                    "0. Izlaz iz programa\n" +
+                    "- - - - - - - - - - - - - - - - - - - - - -\n" +
+                    "- - - - - - - - - - - - - - - - - - - - - -\n" +
+                    "Vaš odabir: ");
+                izbor = int.Parse(Console.ReadLine());
+                switch (izbor)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Unesi ime i prezime zaposlenika: ");
+                        string ip = Console.ReadLine();
+                        Zaposlenik noviZaposlenik = new Zaposlenik(id, ip);
+                        listaZaposlenika.Add(noviZaposlenik);
+                        id++;
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("Pogrešan unos! Pokušajte ponovno!\n");
+                        break;
+                }
+            } while (izbor != 0);
+        }
+    }
+}
---
+++ zaposlenici/zaposlenici/Zaposlenik.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zaposlenici
+{
+    class Zaposlenik
+    {
+        public int Id { get; set; }
+        public string ImePrezime { get; set; }
+        public List<double> MjesecnePlace { get; set; }
+        public Zaposlenik (int id, string imeprezime)
+        {
+            Id = id;
+            ImePrezime = imeprezime;
+        }
+        public void DodajPlacu(double Placa)
+        {
+            MjesecnePlace.Add(Placa);
+        }
+        public double IzracunUkupnePlace()
+        {
+            double Ukupno = 0;
+            foreach (double placa in MjesecnePlace)
+                Ukupno += placa;
+            return Ukupno;
+        }
+        public double IzracunProsjecneZarade()
+        {
+            double Prosjek = 0;
+            int brojac = 0;
+            for (int i=0; i<MjesecnePlace.Count; i++)
+            {
+                if (MjesecnePlace[i] != 0)
+                {
+                    brojac++;
+                    Prosjek += MjesecnePlace[i];
+                }
+            }
+            return Prosjek/brojac;
+        }
+    }
+}
